951b1f15ca366703f5d86e01f02d265d49433102
sreeja-akula <68700978+sreeja-akula@users.noreply.github.com>
2022-06-30 11:22:11 -0400

    new view controllers for each page & labels for the topics they correspond to. we can fix the font and stuff, i just added them in really quick. attached the new view controllers to swift files

---
+++ b4project/HeartPage.swift
@@ -0,0 +1,30 @@
+//
+//  HeartPage.swift
+//  b4project
+//
+//  Created by Admin on 6/30/22.
+//
+
+import UIKit
+
+class HeartPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet weak var heartPageLabel: UILabel!
+    
+}
