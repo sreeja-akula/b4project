291005a891bd47678796191c1233552b31fd6106
sreeja-akula <68700978+sreeja-akula@users.noreply.github.com>
2022-06-29 21:38:59 -0400

    edited "let's get started" button & connected it created new swift file for 2nd view controller added topics page title & created the buttons for each of the topics. connected all of this

---
+++ b4project/Second.swift
@@ -0,0 +1,39 @@
+//
+//  Second.swift
+//  b4project
+//
+//  Created by Admin on 6/29/22.
+//
+
+import UIKit
+
+class Second: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet weak var topics: UILabel!
+    
+    @IBAction func brainButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func heartButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func kidneysButton(_ sender: UIButton) {
+    }
+    
+}
--- b4project/ViewController.swift
+++ b4project/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getStartedButton(_ sender: UIButton) {
+    }
+    
 }
 
